4641104ef0ac838dffe64dd366ddbcf261b13fc3
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-05 15:37:51 -0600

    Create GET methods for projects API

---
+++ src/ProjectManager/Controllers/ProjectsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Data;
+
+namespace ProjectManager.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : Controller
+    {
+        // GET api/projects
+        [HttpGet()]
+        public IActionResult GetProjects()
+        {
+            return Ok(InMemoryDataStore.Current.Projects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProject(int id)
+        {
+            // find project
+            var project = InMemoryDataStore.Current.Projects    
+                .FirstOrDefault(p => p.Id == id);
+                
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+    }
+}
