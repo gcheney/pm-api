222f983953c43839ba0a8d131156263fcea12aeb
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-08 12:44:00 -0600

    Modify developer properties to use Title instead of Level

--- src/ProjectManager/Models/DeveloperDto.cs
+++ src/ProjectManager/Models/DeveloperDto.cs
@@ -5,13 +5,13 @@ namespace ProjectManager.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Level { get; set; }
+        public string Title { get; set; }
 
-        public string FullName 
+        public string FullDetails 
         { 
             get
             {
-                return $"{this.FirstName} {this.LastName}";
+                return $"{this.FirstName} {this.LastName} - {this.Title}";
             }
         }
     }
