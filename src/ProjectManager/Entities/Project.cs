65b9775ffc825cec521376b0fb969770618206ef
Glendon Cheney <glendon.cheney@gmail.com>
2017-03-14 12:59:34 -0500

    Add more data annotation for entity models

--- src/ProjectManager/Entities/Project.cs
+++ src/ProjectManager/Entities/Project.cs
@@ -10,7 +10,12 @@ namespace ProjectManager.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [MaxLength(200)]
         public string Description { get; set; }
         public DateTime ReleaseDate { get; set; }
 
--- src/ProjectManager/Entities/UserStory.cs
+++ src/ProjectManager/Entities/UserStory.cs
@@ -8,12 +8,20 @@ namespace ProjectManager.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [MaxLength(200)]
         public string Description { get; set; }
         public string WorkRemaining { get; set; }
+
+        [Required]
         public bool Completed { get; set; }
 
         public int ProjectId { get; set; }
+        [ForeignKey("ProjectId")]
         public Project Project { get; set; }
     }
 }
